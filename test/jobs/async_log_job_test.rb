4fc6147a96917233cb6a6bacc8dfea7b62486cfa
Tomoaki Hirata <t.hirata.0463@gmail.com>
2021-02-09 14:47:33 +0900

    add test code

--- test/jobs/async_log_job_test.rb
+++ test/jobs/async_log_job_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class AsyncLogJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Enqueue AsyncLogJob" do
+    assert_enqueued_with(job: AsyncLogJob) do
+      AsyncLogJob.perform_later(message: "from test")
+    end
+  end
 end
