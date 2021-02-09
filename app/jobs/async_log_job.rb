c3f8b73cffcb88a7f84122fe6dbfc4e561ed93d8
Tomoaki Hirata <t.hirata.0463@gmail.com>
2021-02-09 14:32:23 +0900

    add sidekiq.yml and job's queues_ad fix

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,14 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :default
+  # queue_as :async_log # defaultキーから変更、config/sidekiq.ymlで設定追加する必要有り
+
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+  end
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)
